ed08a3e18d4fd7a309a2127176201fc031ea2e36
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2023-06-04 22:39:09 +0300

    refactor

--- test_app/app.py
+++ test_app/app.py
@@ -6,13 +6,14 @@ APP = Flask(__name__)
 HOST = '0.0.0.0'
 PORT = 5000
 SERVICE_NAME = os.environ.get('SERVICE_NAME', 'application')
-
+SERVICE_CONTACT = os.environ.get('USERNAME')
 
 @APP.route('/hello/<user>')
 def hello_user(user: str):
     return make_response(
         jsonify(
-            {'message': f'Hello from {SERVICE_NAME},{user}!'}
+            {'message': f'Hello from {SERVICE_NAME},{user}!\n'
+                        f'Support {SERVICE_CONTACT}'}
         ),
         200
     )
