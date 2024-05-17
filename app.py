411274aabb156694a53f246ff62996134468a557
Maksim Porechin <m.porechin@ya.ru>
2024-05-17 12:00:39 +0300

    добавил команду для запуска докера

--- app.py
+++ app.py
@@ -12,11 +12,19 @@ SERVICE_NAME = os.environ.get('SERVICE_NAME', 'application')
 def hello_user(user: str):
     return make_response(
         jsonify(
-            {'message': f'Hello from {SERVICE_NAME},{user}!'}
+            {'message': f'Hello from {SERVICE_NAME}, {user}!'}
         ),
         200
     )
 
 
 if __name__ == '__main__':
+    """
+    sudo docker container run -p5050:5000 -e "SERVICE_NAME=serviceХ" flask_app:v0.1
+    -p5050:5000 - это аргумент принимает 2 числа разделеный :
+    5050 - порт для использования хоста для связи с портом внутри контейнера
+    5000 - на какой порт внутри контейнера перенаправлять запросы порта хоста
+    -e "SERVICE_NAME=service1" - передаем в окружение переменную с именем сервиса
+    serviceХ - имя нашего сервиса, где Х- любое число или имя 
+    """
     APP.run(host=HOST, port=PORT)
